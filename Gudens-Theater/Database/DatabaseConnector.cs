df33692aacbb2fb9eccfa2973f99578297e6420a
testtesttestvoorschool2@gmail.com <testtesttestvoorschool2@gmail.com>
2022-04-08 12:26:17 +0200

    sql connectie werkt

--- Gudens-Theater/Database/DatabaseConnector.cs
+++ Gudens-Theater/Database/DatabaseConnector.cs
@@ -9,7 +9,7 @@ namespace Gudens_Theater.Database
         public static List<Dictionary<string, object>> GetRows(string query)
         {
             // stel in waar de database gevonden kan worden
-            string connectionString = "Server=172.16.160.21;Port=3306;Database=110671;Uid=110671;Pwd=Inf2122sql;";
+            string connectionString = "Server=172.16.160.21;Port=3306;Database=110671;Uid=110671;Pwd=inf2122sql;";
 
             // maak een lege lijst waar we de namen in gaan opslaan
             List<Dictionary<string, object>> rows = new List<Dictionary<string, object>>();
