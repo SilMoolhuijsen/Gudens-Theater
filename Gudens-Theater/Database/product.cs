2396a237e70c12cdf1fd70afc00cd230b0a4af92
110774@st-maartenscollege.nl <110774@st-maartenscollege.nl>
2022-05-11 12:08:04 +0200

    Achtergrond inviduele pagina's verandert

--- Gudens-Theater/Database/product.cs
+++ Gudens-Theater/Database/product.cs
@@ -1,10 +1,12 @@
-﻿namespace Gudens_Theater.Database
+﻿
+
+namespace Gudens_Theater.Database
 {
     public class Product
     {
         public int Id { get; set; }
         public string naam { get; set; }
-
         public string prijs { get; set; }
+        public int Beschikbaarheid { get; set; }
     }
 }
